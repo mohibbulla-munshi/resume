99876557a1b6e6e7c8cef3d7b85f1a810373e71f
Mohibbull Munshi <33421746+mohibbulla-munshi@users.noreply.github.com>
2024-07-28 20:37:16 +0600

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <!-- stylesheet -->
   <link rel="stylesheet" href="style.css">
-  <title>Resume</title>
+  <title>Resume of Mohibbulla Munshi</title>
 </head>
 
 <body>
